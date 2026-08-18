5cae8f9c8f75929445550fab71a8e81839201d71
mrljem <devlemi97@gmail.com>
2026-08-18 23:44:35 +0200

    Minor fixes

--- scripts/main_camera.gd
+++ scripts/main_camera.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
-@export var player := CharacterBody2D
+@export var player: CharacterBody2D
+@export var cameraSpeed: int = 5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +9,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y -= 5 * delta
+	position.y -= cameraSpeed * delta
+
+
+func _on_top_area_body_entered(body: Node2D) -> void:
+	var player = body as CharacterBody2D
+	if player:
+		position.y = move_toward(position.y, player.position.y, 10) 
